a3a81c2891f74ef43e55d3af856772a0cc365a6e
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-07-13 17:34:29 +0530

    New Commits

--- extractor_website/extractor/extractorapp/models.py
+++ extractor_website/extractor/extractorapp/models.py
@@ -1,5 +1,5 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
-
 
 class PDFDocument(models.Model):
     name = models.CharField(max_length=255)
